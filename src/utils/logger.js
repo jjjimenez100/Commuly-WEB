6dfdf3fe91f89602231c34b50eb0c17a12c9a189
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-06 03:00:48 +0800

    #2: Add logging utility for dev environment

---
+++ src/utils/logger.js
@@ -0,0 +1,8 @@
+const loggerFunction = process.env.NODE_V === 'development' ? console.log : () => {};
+
+const log = (...args) => {
+    const mappedArguments = args.map(arg => JSON.stringify(arg, null, 2));
+    loggerFunction.apply(console, mappedArguments);
+};
+
+export { log };
