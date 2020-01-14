f2effb1206cf7608b4a1394bfb21dcd8196a8d5a
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-15 02:05:30 +0800

    #2: Add config folder

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+const API_URL = process.env.API_URL || 'localhost:3000/commuly/api/v1';
+const COMMULY_WEB_VERSION = process.env.COMMULY_WEB_VERSION || 'v1';
+
+export default { API_URL, COMMULY_WEB_VERSION };
