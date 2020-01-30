9f0d8ab5a57a88c1a03c1ecc4508933ee11782b3
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-31 03:42:45 +0800

    #000: Http service for getting cards by team

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -4,6 +4,15 @@ const CardService = {
   createNewContentCard(requestBody, additionalConfig = {}) {
     httpService.post('/card', requestBody, additionalConfig);
   },
+  getCardsByTeam(team, additionalConfig = {}) {
+    const config = {
+      params: {
+        team,
+      },
+      ...additionalConfig,
+    };
+    httpService.get('/cards', config);
+  },
 };
 
 export default CardService;
