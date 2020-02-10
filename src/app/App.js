0bb17bd57c410409aa56c5e4dd7a320ded9f4f18
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-10 21:59:49 +0800

    #45: Add ToastContainer within the root App component

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { routes } from './routes';
 
 class App extends Component {
@@ -15,6 +17,7 @@ class App extends Component {
             })}
           </Switch>
         </Router>
+        <ToastContainer position={toast.POSITION.TOP_CENTER} hideProgressBar />
       </>
     );
   }
