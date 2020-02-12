818646702427a3741b4d766f02546b64ff599bba
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 01:45:00 +0800

    #47: Add ProtectedRoute component

--- src/app/App.js
+++ src/app/App.js
@@ -4,18 +4,31 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { routes } from './routes';
+import ProtectedRoute from './ProtectedRoute';
 
 class App extends Component {
+  mapRoutes = () => {
+    return routes.map(route => {
+      if (route.isPublic) {
+        return <Route key={route.path} {...route} />;
+      }
+
+      return (
+        <ProtectedRoute
+          key={route.path}
+          authenticated
+          path={route.path}
+          component={route.component}
+        />
+      );
+    });
+  };
+
   render() {
     return (
       <>
         <Router>
-          <Switch>
-            {routes.map(route => {
-              if (route.isPublic) return <Route key={route.path} {...route} />;
-              return null;
-            })}
-          </Switch>
+          <Switch>{this.mapRoutes()}</Switch>
         </Router>
         <ToastContainer position={toast.POSITION.TOP_CENTER} hideProgressBar />
       </>
--- src/app/LoggedInView.js
+++ src/app/LoggedInView.js
@@ -1,7 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { loggedInRoutes } from './routes';
 import { Navbar, Sidebar } from '../components';
 
 class LoggedInView extends Component {
@@ -13,6 +11,7 @@ class LoggedInView extends Component {
     this.setState(prevState => ({ isSidebarOpen: !prevState.isSidebarOpen }));
 
   render() {
+    console.log('josh was rendered');
     return (
       <div className="app">
         <Sidebar
@@ -21,13 +20,7 @@ class LoggedInView extends Component {
         />
         <div className="container">
           <Navbar handleSidebarOpen={this.handleSidebarOpen} />
-          <main className="main">
-            <Switch>
-              {loggedInRoutes.map(route => (
-                <Route key={route.path} {...route} />
-              ))}
-            </Switch>
-          </main>
+          <main className="main">{this.props.childComponent}</main>
         </div>
       </div>
     );
