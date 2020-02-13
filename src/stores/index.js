e59310ca66369ecda2244ea48e5ef5b03fe8aa2e
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 22:00:06 +0800

    #46: Add user store to root store of mobx

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,11 @@
 import { types } from 'mobx-state-tree';
+import user, { store as userStore } from './user';
 
-const rootStore = types.model('root', {});
-const store = rootStore.create({});
+const rootStore = types.model('root', {
+  user,
+});
+const store = rootStore.create({
+  user: userStore,
+});
 
 export default store;
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,19 +1,51 @@
 import { types } from 'mobx-state-tree';
+import moment from 'moment-timezone';
+import UserService from '../services/userService';
 
 const User = types
   .model('User', {
     userId: types.string,
     email: types.string,
     token: types.string,
+    role: types.string,
+    expirationDate: types.string,
+    authenticated: types.boolean,
   })
   .actions(self => ({
-    setUserInformation({ userId, email, token }) {
+    setUserInformation({ userId, email, token, role, expirationDate }) {
       self.userId = userId;
       self.email = email;
       self.token = token;
+      self.role = role;
+      self.expirationDate = expirationDate;
+
+      self.authenticated = true;
     },
     setToken(token) {
       self.token = token;
+    },
+    loginUser(userCredentials) {
+      return UserService.loginUser(userCredentials);
+    },
+    userHasSufficientRole(allowedUserRoles = []) {
+      // All user roles are allowed
+      if (allowedUserRoles.length === 0) {
+        return true;
+      }
+      return allowedUserRoles.includes(self.role);
+    },
+    isUserAuthenticated() {
+      if (!self.token) {
+        return false;
+      }
+      const currentTime = moment.tz();
+      const expirationTime = moment.tz(new Date(self.expirationDate), 'Asia/Manila');
+      const tokenIsExpired = currentTime.isBefore(expirationTime);
+
+      return tokenIsExpired;
+    },
+    isUserAllowedForProtectedRoute(allowedUserRoles) {
+      return self.isUserAuthenticated() && self.userHasSufficientRole(allowedUserRoles);
     },
   }));
 
@@ -21,6 +53,9 @@ export const store = User.create({
   userId: '',
   email: '',
   token: '',
+  role: '',
+  expirationDate: '',
+  authenticated: false,
 });
 
-export default store;
+export default User;
