96c7ea0be17e700070af14ccbf0d9b42e2b9f726
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 09:50:44 +0800

    #000: Temporarily hardcode API url

--- src/config/httpService.js
+++ src/config/httpService.js
@@ -1,3 +1,4 @@
-const BASE_URL = process.env.BASE_URL || 'http://localhost:5000/commuly/api/v1';
+// Temporarily hardcode .env variable while still figuring out a way to add it to the amplify environment
+const BASE_URL = 'api.commuly.com:3000/commuly/api/v1/' || 'http://localhost:5000/commuly/api/v1';
 
 export default BASE_URL;
