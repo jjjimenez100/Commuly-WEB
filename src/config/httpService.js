724155694e76fba013decbd308a407c3cad2358d
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 14:26:00 +0800

    #000: Remove unnecessary port number on http request

--- src/config/httpService.js
+++ src/config/httpService.js
@@ -1,5 +1,5 @@
 // Temporarily hardcode .env variable while still figuring out a way to add it to the amplify environment
 const BASE_URL =
-  'https://api.commuly.com:3000/commuly/api/v1/' || 'http://localhost:5000/commuly/api/v1';
+  'https://api.commuly.com/commuly/api/v1/' || 'http://localhost:5000/commuly/api/v1';
 
 export default BASE_URL;
