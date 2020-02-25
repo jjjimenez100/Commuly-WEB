f5e9a76595abb4caf3eef948e085aeca1b41ba0b
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-25 22:05:22 +0800

    #89: Add description on image content card

--- src/components/content-card/ContentImage.js
+++ src/components/content-card/ContentImage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CLOUDFRONT_URL from '../../config/aws';
 
-const ContentImage = ({ imageURLContent }) => (
+// FIXME: Styling on image description
+const ContentImage = ({ imageURLContent, imageDescription }) => (
   <div className="content-image">
     <div className="content-image-overlay" />
     <img src={`${CLOUDFRONT_URL}${imageURLContent}`} alt="url-post" />
+    <br />
+    {imageDescription}
   </div>
 );
 
