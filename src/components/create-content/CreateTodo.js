8b7a5db9b443640fe370d29292891e27d90392e5
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-28 02:04:14 +0800

    #129: Add input validation on create todo content card

--- src/components/create-content/CreateTodo.js
+++ src/components/create-content/CreateTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SimpleReactValidator from 'simple-react-validator';
 import { toast } from 'react-toastify';
 import moment from 'moment';
 import { Typography, Input, Textarea, Button, ModalBody, ModalFooter } from 'components';
@@ -27,6 +28,10 @@ class CreateTodo extends Component {
       startTime: startTime ? moment(startTime, 'HH:mm').format('HH:mm') : moment().format('HH:mm'),
       endTime: endTime ? moment(endTime, 'HH:mm').format('HH:mm') : moment().format('HH:mm'),
     };
+    this.validator = new SimpleReactValidator({
+      className: 'text-danger',
+      autoForceUpdate: this,
+    });
   }
 
   handleInputChanged = e => {
@@ -35,48 +40,52 @@ class CreateTodo extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
-    // add validation for date and time here
-    const { title, description, startDate, startTime, endTime } = this.state;
-    const { team, userId: owner } = getUserDetails();
-    // temporary while we don't have any mockups on adding todos
-    // to selected individuals only
-    const todoType = 'TEAM';
-    const body = {
-      cardType: CONTENT_CARD,
-      contentCardType: TODO_CONTENT,
-      team,
-      owner,
-      todoContent: {
+    if (this.validator.allValid()) {
+      // add validation for date and time here
+      const { title, description, startDate, startTime, endTime } = this.state;
+      const { team, userId: owner } = getUserDetails();
+      // temporary while we don't have any mockups on adding todos
+      // to selected individuals only
+      const todoType = 'TEAM';
+      const body = {
+        cardType: CONTENT_CARD,
+        contentCardType: TODO_CONTENT,
+        team,
+        owner,
+        todoContent: {
+          todoType,
+          title,
+          description,
+          startDate,
+          startTime,
+          endTime,
+        },
         todoType,
-        title,
-        description,
-        startDate,
-        startTime,
-        endTime,
-      },
-      todoType,
-    };
+      };
 
-    const { addCard, updateCard, cardData, onClose } = this.props;
-    try {
-      if (Object.keys(cardData).length === 0) {
-        const { data } = await CardService.createNewContentCard(body);
-        await addCard(data.savedCard);
-        toast.success('Successfully added new content!');
-      } else {
-        const { _id: cardId } = cardData;
-        const {
-          data: { updatedCard },
-        } = await CardService.updateContentCard(cardId, body);
-        updateCard(updatedCard);
-        toast.success('Successfully updated content!');
+      const { addCard, updateCard, cardData, onClose } = this.props;
+      try {
+        if (Object.keys(cardData).length === 0) {
+          const { data } = await CardService.createNewContentCard(body);
+          await addCard(data.savedCard);
+          toast.success('Successfully added new content!');
+        } else {
+          const { _id: cardId } = cardData;
+          const {
+            data: { updatedCard },
+          } = await CardService.updateContentCard(cardId, body);
+          updateCard(updatedCard);
+          toast.success('Successfully updated content!');
+        }
+        onClose();
+      } catch (error) {
+        toast.error('Failed to get a proper response from our services. Please try again later');
+        // eslint-disable-next-line no-console
+        console.log(error);
+        // handle if error, or transfer this whole trycatch to mobx instead
       }
-      onClose();
-    } catch (error) {
-      toast.error('Failed to get a proper response from our services. Please try again later');
-      // eslint-disable-next-line no-console
-      console.log(error);
-      // handle if error, or transfer this whole trycatch to mobx instead
+    } else {
+      this.validator.showMessages();
     }
   };
 
@@ -92,6 +101,7 @@ class CreateTodo extends Component {
             onChange={this.handleInputChanged}
             value={this.state.title}
           />
+          {this.validator.message('title', this.state.title, 'required')}
           <Textarea
             name="description"
             labelText="Description"
@@ -99,6 +109,7 @@ class CreateTodo extends Component {
             onChange={this.handleInputChanged}
             value={this.state.description}
           />
+          {this.validator.message('description', this.state.description, 'required')}
           <div className="create-event-time">
             <Input
               icon={CalendarBlackIcon}
@@ -108,6 +119,7 @@ class CreateTodo extends Component {
               onChange={this.handleInputChanged}
               value={this.state.startDate}
               className="create-todo-date"
+              min={moment().format('YYYY-MM-DD')}
             />
             <Input
               icon={TimerIcon}
@@ -126,6 +138,9 @@ class CreateTodo extends Component {
               value={this.state.endTime}
             />
           </div>
+          {this.validator.message('startDate', this.state.startDate, 'required') ||
+            this.validator.message('startTime', this.state.startTime, 'required') ||
+            this.validator.message('endTime', this.state.endTime, 'required')}
         </ModalBody>
         <ModalFooter>
           <Button size="small" type="button" variant="ghost" onClick={onClose}>
