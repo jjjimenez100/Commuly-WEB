8f2d5cc72a32c0078adb223e22ad50f96e6872e4
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-03-04 01:48:35 +0800

    #87: Add pinning service on commuly-web

---
+++ src/services/teamService.js
@@ -0,0 +1,24 @@
+import httpService from './httpService';
+import { PIN_CARD_TEAM, UNPIN_CARD_TEAM } from '../constants/team';
+
+const TeamService = {
+  pinCardToTeamStream(teamId, cardId, pinType, additionalConfig = {}) {
+    const data = {
+      teamId,
+      cardId,
+      pinType,
+      patchType: PIN_CARD_TEAM,
+    };
+    return httpService.patch(`user/${teamId}`, data, additionalConfig);
+  },
+  unpinCardToTeamStream(teamId, cardId, additionalConfig = {}) {
+    const data = {
+      teamId,
+      cardId,
+      patchType: UNPIN_CARD_TEAM,
+    };
+    return httpService.patch(`user/${teamId}`, data, additionalConfig);
+  },
+};
+
+export default TeamService;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 import httpService from './httpService';
+import { PIN_USER, UNPIN_USER } from '../constants/user';
 
 const UserService = {
   getCardsByUser(userId, additionalConfig = {}) {
@@ -10,8 +11,26 @@ const UserService = {
   loginUser(userCredentials, additionalConfig = {}) {
     return httpService.post('login', userCredentials, additionalConfig);
   },
+  // TODO: Refactor http services to internally use patch type
+  // instead of accepting them as parameters
   markTodo(userId, todoInformation, additionalConfig = {}) {
     return httpService.patch(`user/${userId}/cards`, todoInformation, additionalConfig);
+  },
+  pinCardToUserStream(userId, cardId, additionalConfig = {}) {
+    const data = {
+      userId,
+      cardId,
+      patchType: PIN_USER,
+    };
+    return httpService.patch(`user/${userId}/cards`, data, additionalConfig);
+  },
+  unpinCardToUserStream(userId, cardId, additionalConfig = {}) {
+    const data = {
+      userId,
+      cardId,
+      patchType: UNPIN_USER,
+    };
+    return httpService.patch(`user/${userId}/cards`, data, additionalConfig);
   },
 };
 
